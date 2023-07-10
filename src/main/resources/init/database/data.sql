771c4f9e805266c0351ccbb830ba94ec20ea6994
JunyHarang <junyharang8592@gmail.com>
2023-07-10 15:55:01 +0900

    Discord Bot Token 수정

---
+++ src/main/resources/init/database/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO log_level (log_level_id, level)
+values (1, 'TRACE');
+INSERT INTO log_level (log_level_id, level)
+values (2, 'DEBUG');
+INSERT INTO log_level (log_level_id, level)
+values (3, 'INFO');
+INSERT INTO log_level (log_level_id, level)
+values (4, 'WARN');
+INSERT INTO log_level (log_level_id, level)
+values (5, 'ERROR');
+INSERT INTO log_level (log_level_id, level)
+values (6, 'OFF');
